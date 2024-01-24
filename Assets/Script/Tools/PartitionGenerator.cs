09a3b1be6de6efd8057c316880236449057661ad
Andoni Iturioz <ituriozandoni@gmail.com>
2024-01-24 12:58:51 -0500

    PartitionTool v1

---
+++ Assets/Script/Tools/PartitionGenerator.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PartitionGenerator : MonoBehaviour
+{
+    public Transform[] tracks;
+    public KeyCode[] keyToPress;
+    public KeyCode[] keyToLongPress;
+    public GameObject notePrefab;
+    public GameObject longNotePrefab;
+    public GameObject noteHolder;
+
+    public float beatTempo;
+    bool hasStarted;
+
+    public string prefabPath;
+    public string prefabName;
+    void Start()
+    {
+        beatTempo = beatTempo / 60f;
+    }
+    int id = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            if (Input.anyKeyDown)
+            {
+                hasStarted = true;
+            }
+        }
+        else
+        {
+            noteHolder.transform.position += new Vector3(0, beatTempo * Time.deltaTime, 0);
+        }
+
+        for (int i = 0; i < tracks.Length; i++)
+        {
+            if (Input.GetKeyDown(keyToPress[i]))
+            {
+                GameObject note = Instantiate(notePrefab, tracks[i].position, Quaternion.identity);
+                note.transform.parent = noteHolder.transform;
+                note.name = id.ToString();
+                id++;
+            }
+            if (Input.GetKeyDown(keyToLongPress[i]))
+            {
+                GameObject note = Instantiate(longNotePrefab, tracks[i].position, Quaternion.identity);
+                note.transform.parent = noteHolder.transform;
+            }
+            if (Input.GetKeyUp(keyToLongPress[i]))
+            {
+                GameObject note = Instantiate(longNotePrefab, tracks[i].position, Quaternion.identity);
+                note.transform.parent = noteHolder.transform;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            EndMusic();
+        }
+    }
+
+    void EndMusic()
+    {
+        noteHolder.transform.localScale = new Vector3(1,1,1);
+        noteHolder.GetComponent<BeatScroller>().beatTempo = beatTempo*60;
+        string path = prefabPath +"/"+ prefabName + ".prefab";
+        path = AssetDatabase.GenerateUniqueAssetPath(path);
+        PrefabUtility.SaveAsPrefabAsset(noteHolder, path);
+    }
+}
