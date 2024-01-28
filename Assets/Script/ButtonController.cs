ac632af94266c482a860a1520fbf543382cb55f0
Andoni Iturioz <ituriozandoni@gmail.com>
2024-01-28 01:23:45 -0500

    Character

--- Assets/Script/ButtonController.cs
+++ Assets/Script/ButtonController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.VFX;
 public class ButtonController : MonoBehaviour
 {
     public SpriteRenderer image;
@@ -9,14 +9,21 @@ public class ButtonController : MonoBehaviour
     public KeyCode keyToPress;
 
     [HideInInspector]public bool canBePressed;
+    public VisualEffect VFX;
+    AudioSource SFX;
 
+    private void Start()
+    {
+        SFX = GetComponent<AudioSource>();
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(keyToPress))
         {
             image.enabled = true;
-
+            VFX.Play();
+            SFX.Play();
             if (!canBePressed)
             {
                 GameManager.instance.NoteMissClick();
